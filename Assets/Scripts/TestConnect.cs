5671a4f315efc467206e54053a6f61a602ea8390
Dominic <dominic.viehbock@gmail.com>
2021-03-16 21:43:50 +0100

    creating and listing rooms wip

--- Assets/Scripts/TestConnect.cs
+++ Assets/Scripts/TestConnect.cs
@@ -11,13 +11,17 @@ public class TestConnect : MonoBehaviourPunCallbacks
     void Start()
     {
         print("Connecting to server!!!");
-        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
         print("Connected to server");
+        print(PhotonNetwork.LocalPlayer.NickName);
+
+        PhotonNetwork.JoinLobby();
     }
 
     public override void OnDisconnected(DisconnectCause cause)
